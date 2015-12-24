be1ad087311e0bd5acd6b3e7709c419ff36b5dcc
Jusepe <yosepharegay2@gmail.com>
2015-12-23 23:42:14 -0800

    adding another add methode and calling it on main function

--- Hello.java
+++ Hello.java
@@ -2,5 +2,10 @@ public class Hello{
   public static void main(String[] args){
   //comments of the hello world
   System.out.println("Hello the whole world");
+  System.out.println("The sum of the following two numbers is: 3 and 5" + add(3,5));
  }
+ public static int add(int x, int y){
+     return x + y;
+  }
+
 }
